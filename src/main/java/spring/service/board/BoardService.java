2245b6b4890a6ed527bd29ad1cac30022b84929d
Park-Han-su <hansu0512@gmail.com>
2021-01-15 00:56:29 +0900

    페이징

---
+++ src/main/java/spring/service/board/BoardService.java
@@ -0,0 +1,48 @@
+package spring.service.board;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import spring.common.Page;
+import spring.common.PageCount;
+import spring.dao.board.BoardDAO;
+import spring.dto.board.Board;
+
+@Service
+public class BoardService {
+
+	@Autowired
+	BoardDAO boardDAO;
+	
+	public List<Map> selectBoardAllList(Board board){
+		return boardDAO.selectBoardList(board);
+	}
+	
+	public void readList(Page page, Board board, Model model){
+		if(0 == page.getPageNo()) {
+			page.setPageNo(1);
+		}
+		board.setPageIndex((page.getPageNo()-1) * PageCount.PER_PAGE_CNT);
+		board.setPerPageCount(PageCount.PER_PAGE_CNT);
+		page.setPageCnt(PageCount.getPageCnt(boardDAO.selectCount()));
+		model.addAttribute("boardList", boardDAO.selectBoardList(board));
+		model.addAttribute("page", page);
+	}
+	
+	public int deleteBoard(Board board) {
+		return boardDAO.deleteBoard(board);
+	}
+	
+	public int modifyBoard(Board board) {
+		return boardDAO.modifyBoard(board);
+	}
+	
+	public int insertBoard(Board board) {
+		return boardDAO.insertBoard(board);
+	}
+	
+}
